b46ce8edb20a777036734eb05325dbc0bd1a6928
vitaoXIV <ve569933@gmail.com>
2024-07-10 15:23:00 -0300

    cabamo o php aula

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ $nome_int = (int)$nome; //serve para transfomar, tem varias outras//
 
 echo "<pre>";
 var_dump($nome); //especifica o que ela é//
-echo "<pre>";
+echo "</pre>";
 
  echo rand(0 , 10); //comando que pega um numero aleatorio//
  echo "<br>";
@@ -23,38 +23,58 @@ echo "<pre>";
  //arrays//
  $frutas = array('maça', 'banana', 'manga');
 
- foreach($frutas as $elemento) { //as significa como
+ foreach($frutas as $elemento) { //"as" significa como(uma setinha-->)
     echo "$elemento <br>";
  }
 
- $nomes = array('vitor', 'andre', 'suellen', 'luis');
+ $nomes = array('vitor', 'andre', 'suellen', 'luis'); //"$' + = + array();
 echo "<br>";
+echo $frutas[0];
 
-
- foreach($nomes as $elementos) {
+ foreach($nomes as $elementos) {  //foreach é o melhor para leitura de arrays//
     echo "$elementos <br>";
  }
  echo "<br>";
  
  $idades = array("andre" => 18, "vitor" => 17);
- echo $idades['vitor'];
+ echo $idades['vitor']; //só funciona com aspas simples//
+
 
  foreach($idades as $nomes=>$idades) { //fazendo isso faz as duas frases//
     echo "<br>";
     echo "$nomes tem $idades anos"; //vitor tem 17anos, andre tem 18 anos
  }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Aula php</title>
 </head>
 <body>
-    <h1>Hello World</h1>
+    <h1>OLA MUNDO</h1>
 
 </body>
 </html>
